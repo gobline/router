12dfeeb85aa5cc7f45c90e5f773723e8159996c4
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-02-13 19:35:23 +0100

    Added accessors for RouteData

--- src/RouteData.php
+++ src/RouteData.php
@@ -34,4 +34,31 @@ class RouteData
     {
         return $this->params;
     }
+
+    public function getParam(...$args)
+    {
+        switch (count($args)) {
+            default:
+                throw new \InvalidArgumentException('getParam() takes one or two arguments');
+            case 1:
+                if (!$this->hasParam($args[0])) {
+                    throw new \InvalidArgumentException('Route param "'.$args[0].'" not found');
+                }
+                return $this->params[$name];
+            case 2:
+                if (!$this->hasParam($args[0])) {
+                    return $args[1];
+                }
+                return $this->params[$name];
+        }
+    }
+
+    public function hasParam($name)
+    {
+        if ((string) $name === '') {
+            throw new \InvalidArgumentException('$name cannot be empty');
+        }
+
+        return isset($this->params[$name]);
+    }
 }
