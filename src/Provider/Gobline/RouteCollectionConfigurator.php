30920436a9ca0c73acab1b972d3cb15a9fb6b24b
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-12-07 17:05:07 +0100

    service configurator to automatically detect if literal or placeholder router

--- src/Provider/Gobline/RouteCollectionConfigurator.php
+++ src/Provider/Gobline/RouteCollectionConfigurator.php
@@ -13,20 +13,28 @@ namespace Gobline\Router\Provider\Gobline;
 
 use Gobline\Container\ContainerInterface;
 use Gobline\Container\ServiceConfiguratorInterface;
+use Gobline\Router\Provider\Gobline\LiteralRouteFactory;
+use Gobline\Router\Provider\Gobline\PlaceholderRouteFactory;
 
 /**
  * @author Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
  */
 class RouteCollectionConfigurator implements ServiceConfiguratorInterface
 {
-    public function configure($collection, array $config, ContainerInterface $container)
+    public function configure($collection, array $config)
     {
         $routes = isset($config['routes']) ? $config['routes'] : [];
 
         foreach ($routes as $data) {
-            $factoryClassName = $data['factory'];
+            $factoryClassName = isset($data['factory']) ? $data['factory'] : null;
 
-            $factory = new $factoryClassName();
+            if ($factoryClassName) {
+                $factory = new $factoryClassName();
+            } elseif (strpos($data['path'], ':') !== false) {
+                $factory = new PlaceholderRouteFactory();
+            } else {
+                $factory = new LiteralRouteFactory();
+            }
 
             $route = $factory($data);
 
